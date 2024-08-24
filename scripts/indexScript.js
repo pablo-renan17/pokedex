20a1de81b48f375a303efaaf9953941d9b5a6072
pablo-renan17 <pablonichterwistz@outlook.com>
2024-08-24 00:37:54 -0300

    adicionado evento de clique nos pokemons

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -3,36 +3,53 @@ header = {
     'Content-Type': 'application/json'
 };
 
-async function pegarPokemons(){
+async function pegarTodosPokemons(){
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150",{
         headers: header
     });
     const result = await response.json();
-    console.log(result.results);
-    montarPokemons(result.results);
+    montarPokemons(result.results)
 }
-pegarPokemons();
 
-async function montarPokemons(result){
+async function pegarPokemonImagem(nomePokemon){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}`,{
+        headers: header
+    });
+    let result = await response.json();
+    result = result.sprites.front_default;
+    return result;
+}
 
-    for(let pokemon of result){
-        const resultadoPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`,{
-            headers: header
-        });
-        const responsePokemon = await resultadoPokemon.json();
-        console.log(responsePokemon.sprites);
+function adicionarPokemonLocalStorage(pokemon){
+    localStorage.setItem("pokemonId", pokemon);
+}
+
+function mudarParaPaginaPokemon(){
+    window.location.href = "pokemon.html";
+}
+
+async function montarPokemons(listaPokemon){
+
+    for(let pokemon of listaPokemon){
+        const imagemPokemon = await pegarPokemonImagem(pokemon.name);
 
         const ulPokemons = document.querySelector("ul");
         ulPokemons.insertAdjacentHTML("beforeend",`
-        <li>
+        <li class="li-${pokemon.name}">
             <div>
-                <img src="${responsePokemon.sprites.front_default}">
+                <img src="${imagemPokemon}">
             </div>
             <p>
                 ${pokemon.name}
             </p>
-            
         </li> 
         `);
+        const ulPokemon = document.querySelector(`.li-${pokemon.name}`);
+        ulPokemon.addEventListener("click",()=>{
+            adicionarPokemonLocalStorage(pokemon.name);
+            mudarParaPaginaPokemon();
+        });
     }
 }
+
+pegarTodosPokemons();
